f4f61931c134a3fb24d24529b715123b422b0e35
AmmDuncan <ammielgyanyawson@gmail.com>
2020-10-22 16:27:42 +0000

    nav fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let nav = document.querySelector('.navbar');
 
 document.addEventListener('scroll', () => {
-    if (scrollY > 70 ) {
+    if (scrollY > 50 ) {
         if (!nav.classList.contains('over')){
             nav.classList.add('over')
         }
